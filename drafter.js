ca829cf11dc9b571550f8873acaeb138d5d6d27f
Amol Kapoor <theahura@gmail.com>
2020-09-12 14:28:56 -0400

    Update drafter.js

--- drafter.js
+++ drafter.js
@@ -1,20 +1,23 @@
-const mats = ['engineering', 'mechanical', 'industrial', 'agricultural', 'innovate', 'patriotic', 'militant']
-const allfactions = ['nordic', 'rusviet', 'togawa', 'crimea', 'saxony', 'polania', 'albion']
-const factions = ['nordic', 'rusviet', 'crimea', 'saxony', 'polania']
-const banned = ['patriotic-rusviet', 'mechanical-crimea']
-const use_expansion = false
+const allmats = ['engineering', 'mechanical', 'industrial', 'agricultural', 'innovate', 'patriotic', 'militant'];
+const mats = ['engineering', 'mechanical', 'industrial', 'agricultural', 'patriotic'];
+const allfactions = ['nordic', 'rusviet', 'togawa', 'crimea', 'saxony', 'polania', 'albion'];
+const factions = ['nordic', 'rusviet', 'crimea', 'saxony', 'polania'];
+const banned = ['patriotic-rusviet', 'mechanical-crimea'];
+const use_expansion = false;
 
 function permute(a, b) {
   let new_arr = [];
   for (const i in a) {
     for (const j in b) {
-      new_arr.push(a[i] + '-' + b[j])
+      new_arr.push(a[i] + '-' + b[j]);
     }
   }
     return new_arr;
 }
 
-const permutations = use_expansion ? permute(mats, allfactions) : permute(mats, factions);
+const used_mats = use_expansion ? allmats : mats;
+const used_factions = use_expansion ? allfactions : factions;
+const permutations = permutations(used_mats, used_factions);
 
 function getElements(perms, n) {
   let elements = [];
